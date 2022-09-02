6d50a139da305ab545d29171825f869a042235d0
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2022-09-02 13:35:58 -0500

    Error: Cannot read promise in merge data, state null

--- pages/read/recipes/[fbKey].js
+++ pages/read/recipes/[fbKey].js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { IoIosArrowBack, IoIosAddCircleOutline } from 'react-icons/io';
 import {
