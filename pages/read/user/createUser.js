d146a4482ab20a2673f301ed15afc066748ee63b
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2022-09-20 20:41:48 -0500

    Push up error

--- pages/read/user/createUser.js
+++ pages/read/user/createUser.js
@@ -5,8 +5,8 @@ import {
 } from 'react-bootstrap';
 import { useRouter } from 'next/router';
 import { useAuth } from '../../../utils/context/authContext';
-import { getUser } from '../../../utils/data/apiData/userData';
 import NewUserForm from '../../../components/forms/NewUserForm';
+import { getUser } from '../../../utils/data/apiData/userData';
 
 export default function CreateUser() {
   const { user } = useAuth();
