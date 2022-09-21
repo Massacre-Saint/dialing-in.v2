d233ef5dcf30d769d7d44a89c61919e786e1c81b
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2022-09-20 20:54:16 -0500

    test bug

--- pages/read/user/createUser.js
+++ pages/read/user/createUser.js
@@ -5,7 +5,7 @@ import {
 } from 'react-bootstrap';
 import { useRouter } from 'next/router';
 import { useAuth } from '../../../utils/context/authContext';
-import NewUserForm from '../../../components/forms/NewUserForm';
+import NewUserForm from '../../../components/forms/NewUserForm2';
 import { getUser } from '../../../utils/data/apiData/userData';
 
 export default function CreateUser() {
