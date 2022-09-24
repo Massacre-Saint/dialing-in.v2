c29b38c933409cb4430a1156aacde4a6b8e3013e
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2022-09-24 08:25:04 -0500

    Fixed bug

--- components/buttons/BrewButton.js
+++ components/buttons/BrewButton.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
 
-export default function BrewButton() {
+export default function BrewButton({ recipe }) {
   const router = useRouter();
   const { firebaseKey } = router.query;
   const handleClick = () => {
@@ -11,6 +12,25 @@ export default function BrewButton() {
     });
   };
   return (
-    <button type="button" className="btn-lg btn-span-lg-bottom" onClick={handleClick}>Start Brewing</button>
+    <>
+      {recipe?.completed === true
+        ? (
+          <button type="button" className="btn-lg btn-span-lg-bottom" onClick={handleClick}>Start Brewing</button>
+        )
+        : (
+          ''
+        )}
+    </>
   );
 }
+
+BrewButton.propTypes = {
+  recipe: PropTypes.shape({
+    completed: PropTypes.bool,
+  }),
+};
+BrewButton.defaultProps = {
+  recipe: PropTypes.shape({
+    completed: false,
+  }),
+};
