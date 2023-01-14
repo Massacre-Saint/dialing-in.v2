003601b5ea2d217eddd0d080d8c7183a370de812
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2023-01-14 13:01:33 -0600

    Pushed up bug

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,39 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import { clientCredentials } from './client';
+
+const checkUser = (uid) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/checkuser`, {
+    method: 'POST',
+    body: JSON.stringify({
+      uid,
+    }),
+    headers: {
+      Authorization: uid,
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  })
+    .then((resp) => resolve(resp.json()))
+    .catch(reject);
+});
+
+const registerUser = (userInfo) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/register`, {
+    method: 'POST',
+    body: JSON.stringify({
+      uid: userInfo.uid,
+      image_url: userInfo.imageUrl,
+      name: userInfo.name,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  })
+    .then((resp) => resolve(resp.json()))
+    .catch(reject);
+});
 
 const signIn = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
@@ -10,4 +44,9 @@ const signOut = () => {
   firebase.auth().signOut();
 };
 
-export { signIn, signOut };
+export {
+  checkUser,
+  registerUser,
+  signIn,
+  signOut,
+};
--- utils/data/apiData/userData.js
+++ utils/data/apiData/userData.js
@@ -3,9 +3,26 @@ import { clientCredentials } from '../../client';
 
 const dbUrl = clientCredentials.databaseURL;
 
-const getUser = (uid) => new Promise((resolve, reject) => {
-  axios.get(`${dbUrl}/user/${uid}.json`)
-    .then((response) => resolve(response.data))
+const getUser = (uid, userId) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/users/${userId}`, {
+    headers: {
+      Authorization: uid,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      resolve({
+        id: data.id,
+        uid: data.uid,
+        firstName: data.first_name,
+        lastName: data.last_name,
+        methodId: data.method_id,
+        favRoast: data.fav_roast,
+        favShop: data.fav_shop,
+        description: data.description,
+        imageUrl: data.image_url,
+      });
+    })
     .catch((error) => reject(error));
 });
 
